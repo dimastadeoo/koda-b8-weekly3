930350afedb7f8c9a5d39a8be3a89d55fb602666
dimastadeoo <dimastadeoo@gmail.com>
2026-07-09 00:57:54 +0700

    feat: add welcome message and user input handling in main function

--- main.go
+++ main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
+	"project-golang/feature"
 	"project-golang/menu"
 )
+var welcMess = func(){
+	fmt.Println(`--------------------Selamat Datang di POPEYE------------------`)
+	fmt.Println(`Silahkan Pilih Ingin Pesan Apa:
+	1. Paket Makan
+	2. Makanan
+	3. Minuman
+	4. Lihat Keranjang
+	5. Keluar`)
+	fmt.Println(`--------------------------------------------------------------`)
+}
 
 func main() {
-	
+	welcMess()
+	var choice string
+	fmt.Print("Pilih angka (1-5): ")
+	fmt.Scanln(&choice)
 
-	// Cetak hasil
-	for i, item := range menu.ListMenu() {
-		fmt.Printf("%d: %+v\n", i, item)
-	}
+	feature.FilterMenu(menu.ListMenu(), choice)
+
+
 }
