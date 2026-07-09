4256f55572d29368a3b5eecf4af1818a62e0bcf5
dimastadeoo <dimastadeoo@gmail.com>
2026-07-09 20:58:13 +0700

    refactor: restructure CartProcess and DisplayCart functions for improved clarity and functionality

--- feature/cart.go
+++ feature/cart.go
@@ -11,13 +11,16 @@ type Cart struct {
 	Qty int
 }
 
-func CartProcess(menu menu.User, qty int) []Cart{
+func (c Cart) Subtotal() int{
+	return c.Qty * c.Menu.Price
+
+} 
+
+func CartProcess(menu menu.User, qty int, carts []Cart) []Cart{
 	if qty == 0 {
 		fmt.Println("Input Jumlah Minimal 1")
 		return nil
 	}
-
-	carts := []Cart{}
 
 	for _, i := range carts{
 		if i.Menu.IdMenu == menu.IdMenu{
@@ -38,4 +41,23 @@ func CartProcess(menu menu.User, qty int) []Cart{
 	})
 
 	return carts
+}
+
+func DisplayCart(Carts []Cart) int{
+	totalHarga := 0
+
+	fmt.Println(`--------------------------------------------------------------`)
+	fmt.Println("---------------------Keranjang--------------------------------")
+	
+	for i, dataCart := range Carts{
+		fmt.Printf("%d. %s\n", i+1, dataCart.Menu.Name)
+		fmt.Printf("%dx @ %s\n", dataCart.Qty, FormatRupiah(dataCart.Menu.Price))
+		fmt.Printf("Subtotal: %s\n", FormatRupiah(dataCart.Subtotal()))
+		totalHarga += dataCart.Subtotal()
+	}
+
+	fmt.Println("-------------------------------------------------------------")
+	fmt.Printf("TOTAL HARGA: %s\n", FormatRupiah(totalHarga))
+	fmt.Println("-------------------------------------------------------------")
+	return totalHarga
 }
