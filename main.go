0100a7c76bda8fd9e528506fa04971164db1e3f1
dimastadeoo <dimastadeoo@gmail.com>
2026-07-09 17:55:58 +0700

    refactor: improve formatting and structure in main function for better readability

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"project-golang/feature"
 	"project-golang/menu"
+	"strconv"
 )
-var welcMess = func(){
+
+var welcMess = func() {
 	fmt.Println(`--------------------Selamat Datang di POPEYE------------------`)
 	fmt.Println(`Silahkan Pilih Ingin Pesan Apa:
 	1. Paket Makan
@@ -17,30 +19,49 @@ var welcMess = func(){
 }
 
 func main() {
-	
+	var carts []feature.Cart
+
 	var choice string
 	filMenu := []menu.User{}
-
-	for  {
+	choiceKategori:
+	for {
 		welcMess()
 		fmt.Print("Pilih angka (1-5): ")
 		fmt.Scanln(&choice)
-	
+
 		filMenu = feature.FilterMenu(menu.ListMenu(), choice)
-		if (filMenu != nil){
+		if filMenu != nil {
 			break
 		}
 	}
 
 	menuChoice := menu.User{}
-	for	{
-		fmt.Print("Pilih Menu: ")
+	// choiceDetail:
+	for {
+		fmt.Print("Pilih Nomor Menu / Ketik 0 untuk kembali ke awal: ")
 		fmt.Scanln(&choice)
+		if choice == "0" {
+			goto choiceKategori
+		}
 		menuChoice = feature.DetailMenu(filMenu, choice)
-		if (menuChoice != menu.User{}){
+		if (menuChoice != menu.User{}) {
 			break
 		}
 	}
 
+	for {
+		fmt.Print("Input Jumlah Pesanan: ")
+		fmt.Scanln(&choice)
+		qty, err := strconv.Atoi(choice)
+		if err != nil {
+			fmt.Println("Error: Input tidak valid! Masukkan Jumlah berupa angka saja.")
+			continue
+		}
+		carts = feature.CartProcess(menuChoice, qty)
+		if carts != nil {
+			break
+		}
+
+	}
 
 }
